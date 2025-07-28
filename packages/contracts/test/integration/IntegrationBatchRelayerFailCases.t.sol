476d4d46c8d02444222cee0a439b72c56dc28b7c
0xAustrian <114922365+0xAustrian@users.noreply.github.com>
2025-07-28 13:12:41 -0300

    test: add structure for revert test cases

---
+++ packages/contracts/test/integration/IntegrationBatchRelayerFailCases.t.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.28;
+
+import {IntegrationBase} from './IntegrationBase.sol';
+import {IBatchRelayer} from 'interfaces/IBatchRelayer.sol';
+import {InternalLeanIMT, LeanIMTData} from 'lean-imt/InternalLeanIMT.sol';
+
+contract IntegrationBatchRelayerFailCases is IntegrationBase {
+  using InternalLeanIMT for LeanIMTData;
+
+  uint256 public constant FIVE_PERCENT = 500;
+  Commitment internal _commitment1;
+  Commitment internal _commitment2;
+  address internal _CARL;
+
+  function setUp() public override {
+    super.setUp();
+
+    _CARL = makeAddr('carl');
+
+    // Alice deposits 10 ETH
+    _commitment1 = _deposit(
+      DepositParams({depositor: _ALICE, asset: _ETH, amount: 10 ether, nullifier: 'nullifier_1', secret: 'secret_1'})
+    );
+
+    // Alice deposits 20 ETH
+    _commitment2 = _deposit(
+      DepositParams({depositor: _ALICE, asset: _ETH, amount: 20 ether, nullifier: 'nullifier_2', secret: 'secret_2'})
+    );
+
+    // Push ASP root with label included
+    vm.prank(_POSTMAN);
+    _entrypoint.updateRoot(_shadowASPMerkleTree._root(), 'ipfs_cid_ipfs_cid_ipfs_cid_ipfs_cid_ipfs_cid_ipfs_cid');
+  }
+
+  function test_batchRelayExtraUnrelatedWithdrawal() public {
+    // Bob withdraws the total amount of Alice's commitment, but relayer includes an extra withdrawal for Carl
+    WithdrawalParams[] memory _params = new WithdrawalParams[](2);
+    _params[0] = WithdrawalParams({
+      withdrawnAmount: _commitment1.value,
+      newNullifier: 'nullifier_1a',
+      newSecret: 'secret_1a',
+      recipient: _BOB,
+      commitment: _commitment1,
+      revertReason: NONE
+    });
+    _params[1] = WithdrawalParams({
+      withdrawnAmount: _commitment2.value,
+      newNullifier: 'nullifier_2a',
+      newSecret: 'secret_2a',
+      recipient: _CARL,
+      commitment: _commitment2,
+      revertReason: NONE
+    });
+    _withdrawThroughBatchRelayer(
+      address(_batchRelayer),
+      _params,
+      IBatchRelayer.BatchRelayData({recipient: _BOB, feeRecipient: _RELAYER, relayFeeBPS: FIVE_PERCENT, batchSize: 1}),
+      NONE
+    );
+  }
+
+  function test_batchRelayReplaceWithUnrelatedWithdrawal() public {
+    vm.skip(true);
+  }
+
+  function test_batchRelayWrongProcessooor() public {
+    vm.skip(true);
+  }
+}
