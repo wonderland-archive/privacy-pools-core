1a8dcb04a96fd7093de835fa87f9847ae6a5510c
0xAustrian <114922365+0xAustrian@users.noreply.github.com>
2025-07-28 19:29:22 -0300

    test: add new test case

--- packages/contracts/test/integration/IntegrationBatchRelayerFailCases.t.sol
+++ packages/contracts/test/integration/IntegrationBatchRelayerFailCases.t.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.28;
 
 import {IntegrationBase} from './IntegrationBase.sol';
+import {ProofLib} from 'contracts/lib/ProofLib.sol';
 import {IBatchRelayer} from 'interfaces/IBatchRelayer.sol';
 import {IPrivacyPool} from 'interfaces/IPrivacyPool.sol';
 import {InternalLeanIMT, LeanIMTData} from 'lean-imt/InternalLeanIMT.sol';
@@ -62,7 +63,85 @@ contract IntegrationBatchRelayerFailCases is IntegrationBase {
   }
 
   function test_batchRelayReplaceWithUnrelatedWithdrawal() public {
-    vm.skip(true);
+    ProofLib.WithdrawProof[] memory _proofs = new ProofLib.WithdrawProof[](2);
+
+    IBatchRelayer.BatchRelayData memory _bobRelayData =
+      IBatchRelayer.BatchRelayData({recipient: _BOB, feeRecipient: _RELAYER, relayFeeBPS: FIVE_PERCENT, batchSize: 2});
+    IBatchRelayer.BatchRelayData memory _carlRelayData =
+      IBatchRelayer.BatchRelayData({recipient: _CARL, feeRecipient: _RELAYER, relayFeeBPS: FIVE_PERCENT, batchSize: 2});
+
+    IPrivacyPool.Withdrawal memory _bobWithdrawal =
+      IPrivacyPool.Withdrawal({processooor: address(_batchRelayer), data: abi.encode(_bobRelayData)});
+    IPrivacyPool.Withdrawal memory _carlWithdrawal =
+      IPrivacyPool.Withdrawal({processooor: address(_batchRelayer), data: abi.encode(_carlRelayData)});
+
+    // Carl deposits 10 ETH
+    Commitment memory _commitment3 = _deposit(
+      DepositParams({depositor: _CARL, asset: _ETH, amount: 10 ether, nullifier: 'nullifier_3', secret: 'secret_3'})
+    );
+
+    // Push ASP root with label included
+    vm.prank(_POSTMAN);
+    _entrypoint.updateRoot(_shadowASPMerkleTree._root(), 'ipfs_cid_ipfs_cid_ipfs_cid_ipfs_cid_ipfs_cid_ipfs_cid');
+
+    Commitment memory _newCommitment1;
+    Commitment memory _newCommitment3;
+
+    // Generate proof from alice commitment
+    _newCommitment1.value = _commitment1.value;
+    _newCommitment1.label = _commitment1.label;
+    _newCommitment1.nullifier = _genSecretBySeed('nullifier_1a');
+    _newCommitment1.secret = _genSecretBySeed('secret_1a');
+    _newCommitment1.precommitment = _hashPrecommitment(_commitment1.nullifier, _commitment1.secret);
+    _newCommitment1.hash = _hashCommitment(_newCommitment1.value, _newCommitment1.label, _newCommitment1.precommitment);
+    _newCommitment1.asset = _commitment1.asset;
+
+    // Generate proof from carl commitment
+    _newCommitment3.value = _commitment3.value;
+    _newCommitment3.label = _commitment3.label;
+    _newCommitment3.nullifier = _genSecretBySeed('nullifier_3a');
+    _newCommitment3.secret = _genSecretBySeed('secret_3a');
+    _newCommitment3.precommitment = _hashPrecommitment(_commitment3.nullifier, _commitment3.secret);
+    _newCommitment3.hash = _hashCommitment(_newCommitment3.value, _newCommitment3.label, _newCommitment3.precommitment);
+    _newCommitment3.asset = _commitment3.asset;
+
+    // Generate withdrawal proof
+    ProofLib.WithdrawProof memory _proof1 = _generateWithdrawalProof(
+      WithdrawalProofParams({
+        existingCommitment: _commitment1.hash,
+        withdrawnValue: 0,
+        context: uint256(keccak256(abi.encode(_bobWithdrawal, _ethPool.SCOPE()))) % SNARK_SCALAR_FIELD,
+        label: _commitment1.label,
+        existingValue: _commitment1.value,
+        existingNullifier: _commitment1.nullifier,
+        existingSecret: _commitment1.secret,
+        newNullifier: _newCommitment1.nullifier,
+        newSecret: _newCommitment1.secret
+      })
+    );
+
+    // Generate withdrawal proof
+    ProofLib.WithdrawProof memory _proof2 = _generateWithdrawalProof(
+      WithdrawalProofParams({
+        existingCommitment: _commitment3.hash,
+        withdrawnValue: 0,
+        context: uint256(keccak256(abi.encode(_carlWithdrawal, _ethPool.SCOPE()))) % SNARK_SCALAR_FIELD,
+        label: _commitment3.label,
+        existingValue: _commitment3.value,
+        existingNullifier: _commitment3.nullifier,
+        existingSecret: _commitment3.secret,
+        newNullifier: _newCommitment3.nullifier,
+        newSecret: _newCommitment3.secret
+      })
+    );
+
+    _proofs[0] = _proof1;
+    _proofs[1] = _proof2;
+
+    // Call batch relayer with both proofs (both have different context)
+    vm.prank(_RELAYER);
+    vm.expectRevert(IPrivacyPool.ContextMismatch.selector);
+    _batchRelayer.batchRelay(_ethPool, _bobWithdrawal, _proofs);
   }
 
   function test_batchRelayWrongProcessooor() public {
