e8f91b9435f8a2618f6e1e793dbe2fce67f41985
0xAustrian <114922365+0xAustrian@users.noreply.github.com>
2025-07-29 13:18:56 -0300

    fix: fmt

--- packages/contracts/test/integration/IntegrationBatchRelayerDeployScript.t.sol
+++ packages/contracts/test/integration/IntegrationBatchRelayerDeployScript.t.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.28;
 
-import {DeployBatchRelayer} from 'script/BatchRelayer.s.sol';
-import {IBatchRelayer} from 'interfaces/IBatchRelayer.sol';
 import {Test} from 'forge-std/Test.sol';
+import {IBatchRelayer} from 'interfaces/IBatchRelayer.sol';
+import {DeployBatchRelayer} from 'script/BatchRelayer.s.sol';
 
 contract IntegrationBatchRelayerDeployScript is DeployBatchRelayer, Test {
-    function setUp() public {
-        vm.createSelectFork(vm.rpcUrl('mainnet'));
-    }
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('mainnet'));
+  }
 
-    function test_deployScript() public {
-        IBatchRelayer _batchRelayer = _deployBatchRelayer();
+  function test_deployScript() public {
+    IBatchRelayer _batchRelayer = _deployBatchRelayer();
 
-        assertEq(_batchRelayer.MAX_RELAY_FEE_BPS(), 500);
-    }
+    assertEq(_batchRelayer.MAX_RELAY_FEE_BPS(), 500);
+  }
 }
