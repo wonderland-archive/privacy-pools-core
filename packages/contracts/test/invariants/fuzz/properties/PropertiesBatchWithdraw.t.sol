c0ffeed85cda67d72c0f8d8e47fb91f1c2784a3d
drgorillamd <83670532+drgorillamd@users.noreply.github.com>
2025-08-07 12:16:05 +0200

    feat: properties

--- packages/contracts/test/invariants/fuzz/properties/PropertiesBatchWithdraw.t.sol
+++ packages/contracts/test/invariants/fuzz/properties/PropertiesBatchWithdraw.t.sol
@@ -11,20 +11,21 @@ import {IEntrypoint} from 'interfaces/IEntrypoint.sol';
 contract PropertiesBatchWithdraw is HandlersParent {
   /// @custom:invariant batch relayer balance should never be positive
   /// @custom:invariant-id BATCH-1
-  function property_batch_1() public {}
+  function property_batch_1() public {
+    assertEq(token.balanceOf(address(batchRelayer)), 0, 'BATCH-1');
+    assertEq(payable(batchRelayer).balance, 0, 'BATCH-1');
+  }
 
   /// @custom:invariant processing fee should be paid as the fee taken on the sum of individual withdrawals, `sum(withdrawal amount)*relayer fee / 10_000`
   /// @custom:invariant-id BATCH-2
-  function property_batch_2() public {}
-
   /// @custom:invariant recipient balance should be the sum of individual withdrawals recipient balance (minus fees), `sum(withdrawal amount) - sum(withdrawal amount)*relayer fee / 10_000`
   /// @custom:invariant-id BATCH-3
-  function property_batch_3() public {}
-
-  function handler_batchWithdraw(uint256 _seed) public {
+  function property_batch_2_3(uint256 _seed) public {
     address _caller = address(currentActor());
     uint256 _numberOfCommitments = ghost_depositsOf[_caller].length;
-    uint256 _totalWithdraw;
+
+    uint256 _balanceCallerBefore = token.balanceOf(_caller);
+    uint256 _balanceProcessingFeeRecipientBefore = token.balanceOf(ghost_processingFeeRecipient);
 
     // solhint-disable-next-line
     if (_numberOfCommitments == 0) revert();
@@ -65,18 +66,24 @@ contract PropertiesBatchWithdraw is HandlersParent {
 
     // Postconditions
     if (success) {
-      assertEq(token.balanceOf(address(batchRelayer)), 0, 'BATCH-1');
-
-      delete ghost_depositsOf[_caller];
-
+      uint256 _batchWithdraw;
       for (uint256 i = 0; i < _numberOfCommitments; i++) {
         ghost_nullifier_used[++_firstNullifier] = true;
         ghost_allCommitments.push(_firstNullifier);
-
-        _totalWithdraw += _deposits[i].depositAmount;
+        _batchWithdraw += _deposits[i].depositAmount;
       }
+      _updateGhostAccountingWithdraw(_batchWithdraw);
 
-      _updateGhostAccountingWithdraw(_totalWithdraw);
+      uint256 _fee = _batchWithdraw * FEE_PROCESSING / 10_000;
+
+      assertEq(token.balanceOf(address(batchRelayer)), 0, 'BATCH-1');
+      assertEq(payable(batchRelayer).balance, 0, 'BATCH-1');
+
+      assertEq(_balanceProcessingFeeRecipientBefore + _fee, token.balanceOf(ghost_processingFeeRecipient), 'BATCH-2');
+
+      assertEq(_balanceCallerBefore + _batchWithdraw - _fee, token.balanceOf(address(currentActor())), 'BATCH-3');
+
+      delete ghost_depositsOf[address(currentActor())];
     } else {
       assertTrue(!mockVerifier.validProof(), 'non-revert: withdraw (1)');
     }
